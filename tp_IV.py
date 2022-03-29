6c02303df9a77b87c4ca1314f2d81f3d673792b5
bentosmariano33 <101131925+bentosmariano33@users.noreply.github.com>
2022-03-28 21:46:52 -0300

    Punto 2 tp4

--- tp_IV.py
+++ tp_IV.py
@@ -1,12 +1,17 @@
 #1. Cree una función que reciba dos números como parámetro, y muestre en pantalla la suma aritmética 
-# de ambos. Invoque a la función con dos números leídos desde teclado. 
+# de ambos. Invoque a la función con dos números leídos desde teclado.
+# #2. Modifique el script del ejercicio anterior para que la función retorne el resultado en vez de mostrarlo. 
+#El programa debe seguir mostrando el resultado en pantalla. 
 
 num1 = int(input("Ingrese un número entero: "))
 num2 = int(input("Ingrese otro número entero: "))
 
 def suma(a, b):
     resultado = a+b
-    print(resultado)
+    return resultado
 
-suma(num1,num2)
+print(suma(num1,num2))
+
+
+ 
         
