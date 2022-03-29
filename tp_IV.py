91f14ca0be90abb45e0e8ff025e290fa85b86352
bentosmariano33 <101131925+bentosmariano33@users.noreply.github.com>
2022-03-29 14:06:22 -0300

    Punto 3 realizado

--- tp_IV.py
+++ tp_IV.py
@@ -10,7 +10,22 @@ def suma(a, b):
     resultado = a+b
     return resultado
 
-print(suma(num1,num2))
+print(suma(num1,num2)) #con esta función se muestra el resultado en pantalla luego de retornarlo.
+
+# 3. Cree una función que reciba un string como parámetro, y retorne la cantidad de letras que posee. 
+# Luego, utilice la función para escribir un programa que solicite ingresar el nombre del usuario, 
+# y luego muestre en pantalla cuántas letras tiene ese nombre.
+
+def palabra(p):
+    cont_letras = len(p)
+    return cont_letras
+
+tu_nombre = input("Dime tu nombre: ")
+print(palabra(tu_nombre))
+
+
+
+
 
 
  
