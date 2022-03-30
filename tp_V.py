d0b71f40076b879a9d7d6fa8712e333fcd6dcf7c
bentosmariano33 <101131925+bentosmariano33@users.noreply.github.com>
2022-03-29 22:46:18 -0300

    Punto 8

--- tp_V.py
+++ tp_V.py
@@ -58,6 +58,12 @@ def nombres(nombre1, nombre2):
 
 print(nombres("Martin", "Jose"))
 
+# 8. Cree un archivo llamado modulo_cadena.py; dentro de él, 
+# cree una función llamada leer_cadena que, sin recibir ningún parámetro, 
+# le solicite al usuario leer un string cualquiera, y luego lo retorne. 
+# Luego cree otro archivo llamado programa_principal.py, que ejecute el programa haciendo uso de la función
+# creada en el otro archivo.
+
 
 
 
