f43f7c31b17c4980550da9318ff3c39dfde5a168
bentosmariano33 <101131925+bentosmariano33@users.noreply.github.com>
2022-04-04 19:08:45 -0300

    El test funciona correctamente

--- modulo_cadena.py
+++ modulo_cadena.py
@@ -2,5 +2,13 @@ def leer_cadena():
     string = input("Ingrese su texto: ")
     return string
 
+def test_leer_cadena():
+    print("Testeando la función leer_cadena()...")
+    assert leer_cadena() == "Marcos"
+    assert leer_cadena() == "Matias"
+    assert leer_cadena() == "Mariano"
+    print("Pasó!")
+
+test_leer_cadena()
 
     
