4dad75f87e2636be09e81d8e41f55d2311d3cd67
bentosmariano33 <101131925+bentosmariano33@users.noreply.github.com>
2022-05-08 19:56:17 -0300

    Actualizado 08/05

--- Ciclos.py
+++ Ciclos.py
@@ -1,100 +1,100 @@
 # from re import X
 
 
-def varios_numeros():
-    cantidad = int(input("Cuantos números quieres procesar?: "))
-    for j in range(0, cantidad):
-        numero = int(input("Ingrese un número: "))
-        if numero > 0:
-            print("Número positivo")
-        elif numero == 0:
-            print("Igual a 0")
-        else:
-            print("Número negativo")
+# def varios_numeros():
+#     cantidad = int(input("Cuantos números quieres procesar?: "))
+#     for j in range(0, cantidad):
+#         numero = int(input("Ingrese un número: "))
+#         if numero > 0:
+#             print("Número positivo")
+#         elif numero == 0:
+#             print("Igual a 0")
+#         else:
+#             print("Número negativo")
 
 
 
-varios_numeros()
+# varios_numeros()
 
 
-def datos():
-    hay_mas_datos = "Si"
-    while hay_mas_datos == "Si":
-        x = int(input("Ingrese un número: "))
-        if x > 0:
-            print("Numero positivo")
-        elif x == 0:
-            print("Igual a 0")
-        else:
-            print("Numero negativo")
-        hay_mas_datos = input("Quiere seguir? <Si-No>: ")
+# def datos():
+#     hay_mas_datos = "Si"
+#     while hay_mas_datos == "Si":
+#         x = int(input("Ingrese un número: "))
+#         if x > 0:
+#             print("Numero positivo")
+#         elif x == 0:
+#             print("Igual a 0")
+#         else:
+#             print("Numero negativo")
+#         hay_mas_datos = input("Quiere seguir? <Si-No>: ")
 
-datos()
+# datos()
 
-# -------------------------------------------------------------------------------------------
-#Forma correcta de programar#
-#Ciclo interactivo#
+# # -------------------------------------------------------------------------------------------
+# #Forma correcta de programar#
+# #Ciclo interactivo#
 
-def pcn(x):
-    if x > 0:
-        resultado = "Número positivo"
-    elif x == 0:
-        resultado = "Igual a 0"
-    else:
-        resultado = "Número negativo"
-    return resultado
+# def pcn(x):
+#     if x > 0:
+#         resultado = "Número positivo"
+#     elif x == 0:
+#         resultado = "Igual a 0"
+#     else:
+#         resultado = "Número negativo"
+#     return resultado
 
-def muchos_pcn():
-    hay_mas_datos = "Sí"
-    while hay_mas_datos == "Sí":
-        x = int(input("Ingrese un número: "))
-        print(pcn(x))
-        hay_mas_datos = input("Quiere seguir? <Sí-No>: ")
+# def muchos_pcn():
+#     hay_mas_datos = "Sí"
+#     while hay_mas_datos == "Sí":
+#         x = int(input("Ingrese un número: "))
+#         print(pcn(x))
+#         hay_mas_datos = input("Quiere seguir? <Sí-No>: ")
 
-# Test
+# # Test
 
-# def test_pcn():
-#     assert pcn(0) == "Igual a 0", "Debe ser igual a 0"
-#     assert pcn(1) == "Número positivo", "Debe ser un numero positivo"
-#     assert pcn(10) == "Número positivo", "Debe ser un numero positivo"
-#     assert pcn(-1) == "Número negativo", "Debe ser un numero negativo"
-#     assert pcn(-10) == "Número negativo", "Debe ser un numero negativo"
-#     print("Función pcn testeada exitosamente.")
+# # def test_pcn():
+# #     assert pcn(0) == "Igual a 0", "Debe ser igual a 0"
+# #     assert pcn(1) == "Número positivo", "Debe ser un numero positivo"
+# #     assert pcn(10) == "Número positivo", "Debe ser un numero positivo"
+# #     assert pcn(-1) == "Número negativo", "Debe ser un numero negativo"
+# #     assert pcn(-10) == "Número negativo", "Debe ser un numero negativo"
+# #     print("Función pcn testeada exitosamente.")
 
-# test_pcn()
+# # test_pcn()
 
 
-# Ciclo con centinela
+# # Ciclo con centinela
 
-def pcn(x):
-    if x > 0:
-        resultado = "Número positivo"
-    elif x == 0:
-        resultado = "Igual a 0"
-    else:
-        resultado = "Número negativo"
-    return resultado
+# def pcn(x):
+#     if x > 0:
+#         resultado = "Número positivo"
+#     elif x == 0:
+#         resultado = "Igual a 0"
+#     else:
+#         resultado = "Número negativo"
+#     return resultado
 
-def leer_centinela():
-    return input("ingrese un numero (* para terminar): ")
+# def leer_centinela():
+#     return input("ingrese un numero (* para terminar): ")
 
-def muchos_pcn():
-    centinela = leer_centinela()
-    while centinela != "*":
-        x = int(centinela)
-        print(pcn(x))
-        centinela = leer_centinela()
+# def muchos_pcn():
+#     centinela = leer_centinela()
+#     while centinela != "*":
+#         x = int(centinela)
+#         print(pcn(x))
+#         centinela = leer_centinela()
 
-# muchos_pcn()
+# # muchos_pcn()
 
-# for i in range(100):
-#     print(i)
+# # for i in range(100):
+# #     print(i)
 
-# Ejercicio guiado:
+# # Ejercicio guiado:
 
-for j in range(1, 11):
-    if (j % 2) == 1:
-        print(j)
+# for j in range(1, 11):
+#     if (j % 2) == 1:
+#         print(j)
     
 # cantidad = 3
 # suma = 0
@@ -115,5 +115,18 @@ for j in range(1, 11):
 #    nota = int(input("Ingrese nota: "))
 #    suma_notas += nota
 
+cantidad_notas = 0
+sumatoria = 0
+numero = int(input("Ingrese un número entero: "))
 
-   
+while numero != -1:
+    sumatoria += numero
+    multiplicacion = numero * 2
+    cantidad_notas +=1
+    print(multiplicacion)
+    numero = int(input("Ingrese un número entero (si quiere terminar ingrese -1): "))
+
+if cantidad_notas > 0:
+    promedio = sumatoria / cantidad_notas
+    print(promedio)
+print(sumatoria)
---
+++ TP_VIII.py
@@ -0,0 +1,46 @@
+# 1. Cree un script que le pida al usuario ingresar palabras, una a una, hasta que el
+# usuario ingrese la palabra “parar”. A medida que se van ingresando las
+# palabras, el programa simplemente debe mostrarlas en pantalla. Al detectar la
+# palabra para detenerse, debe mostrar el mensaje “--- TERMINADO ---”.
+
+# palabra = " "
+# while palabra != "parar":
+#     palabra = input("Ingrese una palabra (o parar para terminar): ")
+#     print(palabra)
+#     if palabra == "parar":
+#         print("---TERMINADO---")
+
+# 2. Cree un script que le solicite al usuario ingresar notas de parciales por teclado,
+# hasta que el usuario ingrese el valor -1, indicando que ya no hay más notas para
+# cargar. Una vez ingresadas las notas, el programa debe informar la nota
+# promedio (tenga cuidado de no incluir al -1 dentro del promedio).
+
+# cantidad_notas = 0
+# suma_notas = 0
+# nota = int(input(f"Ingrese nota (o -1 para finalizar): "))
+
+# while nota != -1:
+#     suma_notas += nota
+#     cantidad_notas += 1
+#     nota = int(input("Ingrese nota (o -1 para finalizar): "))
+#     if nota == -1:
+#         print("No hay más notas para cargar")
+
+# if cantidad_notas > 0:        
+#     promedio = (suma_notas/cantidad_notas)
+#     print("El promedio de las notas es ",promedio)
+
+# 3. Cree un script que le solicite al usuario leer un número entero entre 1 y 100. El
+# programa debe ser capaz de solicitarle al usuario que reingrese el número
+# cuantas veces sea necesario, hasta que el usuario provea un dato válido. Cada
+# vez que detecte un error de validación, informele al usuario cuál fue el error, con
+# los mensajes “El dato ingresado no es numérico.”, o “El número ingresado está
+# fuera del rango permitido.”. Finalmente, cuando el usuario ingrese un dato
+# válido, muestre el mensaje “[NÚMERO] es válido. Gracias!”.
+
+# valor = (input("Ingrese un número entre 1 y 100: "))
+
+# while (valor >= 1) and (valor <= 100):
+
+numero = (input("Ingrese un número entre 1 y 100: "))
+
